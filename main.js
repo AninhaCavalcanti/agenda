4988e31a192599621873c78241cc1fe6f57a4117
Ana Maria <aninha.cavalcanti.0123@gmail.com>
2023-07-03 18:49:49 -0300

    commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+const form = document.getElementById('form-contatos');
+
+let linhas = '';
+
+form.addEventListener('submit',function(e) {
+  e.preventDefault();
+
+  const inputNome = document.getElementById('nome');
+  const inputTelefone = document.getElementById('telefone');
+
+  let linha = '<tr>';
+  linha += `<td>${inputNome.value}</td>`;
+  linha += `<td>${inputTelefone.value}</td>`;
+  linha += `</tr>`;
+
+  linhas += linha;
+
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linha;
+
+  inputNome.value = '';
+  inputTelefone.value = '';
+});
